b311f031442aa1c16b041d595439c7499725e876
Dimple Mehta <dimplemehta@MacBook-Pro.local>
2022-11-17 12:50:15 +0530

    Initial commit

---
+++ src/main/java/com/pleaseHesitate/init/model/Content.java
@@ -0,0 +1,14 @@
+package com.pleaseHesitate.init.model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@Builder
+public class Content {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/com/pleaseHesitate/init/service/Greeting.java
@@ -0,0 +1,20 @@
+package com.pleaseHesitate.init.service;
+
+import com.pleaseHesitate.init.model.Content;
+import lombok.Builder;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/home")
+public class Greeting {
+
+    private static int counter=1;
+    private static final String hello = "Hello, Ch %s!";
+
+    @GetMapping("/greeting")
+    public Content greeting(@RequestParam (value ="name", defaultValue = "Hello World")String name)
+    {
+        return Content.builder().id(counter++).name(String.format (hello , name)).build();
+    }
+}
